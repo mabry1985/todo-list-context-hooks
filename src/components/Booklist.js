7b1552757dfe0e39435e520476414d7c6455a552
Josh Mabry <mabry1985@gmail.com>
2020-01-28 11:06:25 -0800

    add bookcontext component

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
-export const Booklist = () => {
+const Booklist = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div
@@ -10,9 +12,11 @@ export const Booklist = () => {
       style={{ color: theme.syntax, background: theme.bg }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>The Way of Kings</li>
-        <li style={{ background: theme.ui }}>The Name of the Wind</li>
-        <li style={{ background: theme.ui }}>The Final Empire</li>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>
+          )
+        })}
       </ul>
     </div>
   );
