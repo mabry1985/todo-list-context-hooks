917c90d12ffaef24ef66601939f5121ae9f4bde3
Josh Mabry <mabry1985@gmail.com>
2020-01-28 08:57:33 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Booklist from './components/Booklist';
+import ThemeContextProvider from './contexts/ThemeContext';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
